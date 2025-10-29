83ca94eb017d682d9897fb2ee8a5d9a680715cb4
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-10-30 00:00:56 +0100

    feat: update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>(() => {})();</h1>
+  <p>Do it.</p>
+  <p>Do what?</p>
+  <p>Do it now.</p>
+  <h2>Timeline</h2>
+  <ul>
+    <li>TAPC</li>
+    <li>BAPC</li>
+    <li>NWERC</li>
+  </ul>
+  <h2>Meet the Team</h2>
+  <ul>
+    <li>
+      <h3>Anton Anapreyenka</h3>
+      <span>Lorem Ipsum</span>
+    </li>
+    <li>
+      <h3>Niladri Adhikary</h3>
+    </li>
+    <li>
+      <h3>Valyn Tyler</h3>
+    </li>
+  </ul>
 </template>
 
 <style scoped></style>
