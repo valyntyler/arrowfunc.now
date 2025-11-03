e949e5457fcfcd1ad66418907b92a694d83a47d8
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-11-03 01:03:28 +0100

    feat: EventItem component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
+import EventItem from "./EventItem.vue";
 import Timer from "./Timer.vue";
 </script>
 
 <template>
   <div class="container">
-    <Timer></Timer>
+    <EventItem
+      e_title="NWERC"
+      e_title_long="ICPC Northwestern Europe Regional Contest"
+    />
+    <EventItem
+      e_title="BAPC"
+      e_title_long="Benelux Algorithm Programming Contest"
+      :e_date="new Date('2025-10-25')"
+    />
+    <EventItem
+      e_title="TAPC"
+      e_title_long="Twentse Algorithm Programming Contest"
+      :e_date="new Date('2025-09-20')"
+    />
   </div>
 </template>
 
---
+++ src/EventItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <li class="event-item">
+    <h3>{{ e_title }}</h3>
+    <p>{{ e_title_long }}</p>
+    <p>{{ moment(e_date).format("MMMM Do YYYY") }}</p>
+  </li>
+</template>
+
+<script setup>
+import moment from "moment";
+
+const props = defineProps({
+  e_title: String,
+  e_title_long: String,
+  e_date: Date,
+});
+</script>
+
+<style scoped>
+.event-item p {
+  color: var(--text-secondary);
+}
+</style>
