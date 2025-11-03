810e6f636fa589483efa071fd793738babc23598
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-11-03 01:12:54 +0100

    refactor: EventList component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,12 @@
 <script setup lang="ts">
 import EventItem from "./EventItem.vue";
+import EventList from "./EventList.vue";
 import Timer from "./Timer.vue";
 </script>
 
 <template>
   <div class="container">
-    <EventItem
-      e_title="NWERC"
-      e_title_long="ICPC Northwestern Europe Regional Contest"
-    />
-    <EventItem
-      e_title="BAPC"
-      e_title_long="Benelux Algorithm Programming Contest"
-      :e_date="new Date('2025-10-25')"
-    />
-    <EventItem
-      e_title="TAPC"
-      e_title_long="Twentse Algorithm Programming Contest"
-      :e_date="new Date('2025-09-20')"
-    />
+    <EventList></EventList>
   </div>
 </template>
 
---
+++ src/EventList.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul class="event-list">
+    <EventItem
+      v-for="event in events"
+      :e_title="event.title"
+      :e_title_long="event.title_long"
+      :e_date="event.date"
+    ></EventItem>
+  </ul>
+</template>
+
+<script setup>
+import EventItem from "./EventItem.vue";
+import { ref } from "vue";
+
+const events = ref([
+  {
+    date: new Date(),
+    title: "NWERC",
+    title_long: "ICPC Northwestern Europe Regional Contest",
+  },
+  {
+    date: new Date("2025-10-25"),
+    title: "BAPC",
+    title_long: "Benelux Algorithm Programming Contest",
+  },
+  {
+    date: new Date("2025-09-20"),
+    title: "TAPC",
+    title_long: "Twentse Algorithm Programming Contest",
+  },
+]);
+</script>
+
+<style scoped></style>
