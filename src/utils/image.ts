5626c44c8f1b2007f9e61dc8d64d7712e8a4403b
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-11-23 23:37:00 +0100

    feat: create image.ts

---
+++ src/utils/image.ts
@@ -0,0 +1,46 @@
+type Vector2 = [number, number];
+
+export function loadImage(path: string, resolution: Vector2) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+
+    img.crossOrigin = "anonymous";
+    img.src = path;
+
+    img.onerror = () => {
+      reject(new Error(`Failed to load image: ${path}`));
+    };
+
+    img.onload = () => {
+      const canvas = document.createElement("canvas");
+      canvas.width = resolution[0];
+      canvas.height = resolution[1];
+
+      const ctx = canvas.getContext("2d")!;
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+      const data = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      resolve(data);
+    };
+  });
+}
+
+export function toImageSpace(position: Vector2, resolution: Vector2) {
+  const width = resolution[0];
+  const x = position[0];
+  const y = position[1];
+  return (y * width + x) * 4;
+}
+
+export function imageRGB(
+  image: ImageData,
+  position: Vector2,
+  resolution: Vector2,
+) {
+  const pixelIndex = toImageSpace(position, resolution);
+  return [
+    image.data[pixelIndex],
+    image.data[pixelIndex + 1],
+    image.data[pixelIndex + 2],
+  ];
+}
