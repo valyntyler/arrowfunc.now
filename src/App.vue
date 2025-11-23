fcaa209d98a78ab70b6fcbaca6efe2b57770c3ca
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-11-23 21:44:14 +0100

    feat: splashtext

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-import EventList from "./EventList.vue";
-import Timer from "./Timer.vue";
+import SplashText from "./SplashText.vue";
 </script>
 
 <template>
   <div class="container">
-    <EventList></EventList>
-    <Timer></Timer>
+    <SplashText></SplashText>
   </div>
 </template>
 
---
+++ src/SplashText.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const lmb = "(() => {})();";
+const url = "arrowfunc.now";
+
+const text = ref("");
+let i = 0;
+
+const typingAnimation = setInterval(() => {
+  text.value += url[i];
+  i++;
+  if (i == url.length) {
+    clearInterval(typingAnimation);
+  }
+}, 50);
+</script>
+
+<template>
+  <h1 class="splash-text">{{ text }}</h1>
+</template>
+
+<style scoped>
+.splash-text {
+  background: red;
+  white-space: pre;
+}
+</style>
