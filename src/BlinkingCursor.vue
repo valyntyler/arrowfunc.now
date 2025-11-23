d6517be90447c4eb7e3998b47fce04eb58718f78
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-11-23 21:52:46 +0100

    feat: blinking cursor

---
+++ src/BlinkingCursor.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const cursor = ref("_");
+
+setInterval(() => {
+  if (cursor.value == "_") {
+    cursor.value = " ";
+  } else {
+    cursor.value = "_";
+  }
+}, 380);
+</script>
+
+<template>
+  <h1 class="blinking-cursor">{{ cursor }}</h1>
+</template>
+
+<style scoped>
+.blinking-cursor {
+  color: magenta;
+  background: yellow;
+  white-space: pre;
+  font-weight: bold;
+}
+</style>
