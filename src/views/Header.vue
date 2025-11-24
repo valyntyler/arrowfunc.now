0920d80208ec4a09e48e2e0ae6633f0f189a23f8
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-11-24 21:27:22 +0100

    temp: update header

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -19,4 +19,28 @@
 .header :last-child {
   color: var(--text-primary-dim);
 }
+
+/**/
+
+.header {
+  text-align: left;
+  margin-bottom: 3rem;
+  border-bottom: 1px solid var(--border-color);
+  padding-bottom: 2rem;
+}
+
+.header h1 {
+  font-size: 2rem;
+  /* font-weight: normal; */
+  margin-bottom: 0.5rem;
+  color: var(--text-primary);
+  text-transform: uppercase;
+  letter-spacing: 0.1em;
+}
+
+.header .subtitle {
+  font-size: 1rem;
+  color: var(--text-secondary);
+  margin-bottom: 1rem;
+}
 </style>
