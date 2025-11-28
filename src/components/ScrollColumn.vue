4d18865e2d6d353ac93281dd5865922eff2eb866
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-11-28 01:09:59 +0100

    feat: smooth keyboard scrolling

--- src/components/ScrollColumn.vue
+++ src/components/ScrollColumn.vue
@@ -1,7 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from "vue";
+
+let index = 0;
+
+onMounted(() => {
+  const main = document.getElementById("main")!;
+  document.addEventListener("keydown", (event) => {
+    const key = event.key.toUpperCase();
+    let scrolling = false;
+    switch (key) {
+      case "J":
+      case "S":
+      case "ArrowDown".toUpperCase():
+        index = Math.min(index + 1, main.children.length - 1);
+        scrolling = true;
+        break;
+      case "K":
+      case "W":
+      case "ArrowUp".toUpperCase():
+        index = Math.max(0, index - 1);
+        scrolling = true;
+        break;
+    }
+    if (scrolling) {
+      main.children[index]?.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
+</script>
 
 <template>
-  <main>
+  <main id="main">
     <slot></slot>
   </main>
 </template>
